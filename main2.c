cf08806a7d92b660d8059dabdcb558d19cfcb49c
lyes hamroun <lyeshamroun@gmail.com>
2018-09-21 18:32:54 +0200

    ajout de strdel mais pas sure de moi

--- main2.c
+++ main2.c
@@ -4,26 +4,33 @@ int		main(int ac, char **av)
 {
 	int i = 0;
 	int j = 0;
+	char **sd;
 
+	sd = NULL;
 	(void)ac;
 	//printf("la mienne : %s\n", ft_itoa(atoi(av[1])));
 
 	//ft_putchar_fd(av[1][0], atoi(av[2]));
 
-	while (ft_strsplit(av[1], av[2][0])[i])
-	{
-		j = 0;
-		while (ft_strsplit(av[1], av[2][0])[i][j])
-			ft_putchar(ft_strsplit(av[1], av[2][0])[i][j++]);
-		ft_putchar('\n');
-		i++;
-	}
+//	while (ft_strsplit(av[1], av[2][0])[i])
+//	{
+//		j = 0;
+//		while (ft_strsplit(av[1], av[2][0])[i][j])
+//			ft_putchar(ft_strsplit(av[1], av[2][0])[i][j++]);
+//		ft_putchar('\n');
+//		i++;
+//	}
 
 //	while (ft_split_whitespaces(av[1])[i])
 //	{
 //		ft_putstr(ft_split_whitespaces(av[1])[i++]);
 //		ft_putchar('\n');
 //	}
+
+	sd = ft_split_whitespaces(av[1]);
+	printf("avant del : %s\n", *sd);
+	ft_strdel(sd);
+	printf("apres del : %s\n", *sd);
 
 	return (0);
 }

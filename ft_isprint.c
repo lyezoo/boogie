f27cd9164d02d878db87961f329411e66320f91e
lyes hamroun <lyeshamroun@gmail.com>
2018-10-15 05:04:36 +0200

    suppr des .o

--- ft_isprint.c
+++
@@ -1,8 +0,0 @@
-#include "lh_proto.h"
-
-int		ft_isprint(int c)
-{
-	if (c >= 32 && c < 127)
-		return (1);
-	return (0);
-}
--- ft_putchar.c
+++
@@ -1,6 +0,0 @@
-#include "lh_proto.h"
-
-void	ft_putchar(int c)
-{
-	write(1, &c, 1);
-}
